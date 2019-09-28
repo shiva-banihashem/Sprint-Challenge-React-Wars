ff3f9657f2e66bf3ddc6007e7c110654310af54d
shiva banihashem <sbanihas@gmail.com>
2019-09-28 12:53:04 -0500

    MVP included

--- Answers.md
+++ Answers.md
@@ -1,11 +1,16 @@
 # Answers
 
 1. What is React JS and what problems does it try and solve? Support your answer with concepts introduced in class and from your personal research on the web.
+React is a JavaScript library built by Facebook to create user interface
+components. It resolves performance issue for updating elements in DOM by using virtual DOM.
 
 1. What does it mean to think in react?
-
+Thinking in React means to think about apps as you build them.
 1. Describe state.
+It is an object where you store property values that belongs to a component.
 
 1. Describe props.
+Props are used to pass data from a parent component to a child component
 
 1. What are side effects, and how do you sync effects in a React component to state or prop changes?
+A side effect is anything that effects something outside the scope of the function being executed. effects are synched by using useEffect hook in React and using the dependency.
--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,40 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import DisplayInfo from './components/DisplayInfo.js';
+import styled,{ keyframes } from 'styled-components';
 
+const Container = styled.div`
+width: 98%;
+display:flex;
+border-box:box-sizing;
+flex-wrap:wrap;
+justify-content: space-evenly;
+margin-top:20%;
+`;
+const rotate = keyframes`
+from {
+  
+  transform: skew(0deg,30deg);
+}
+to {
+ 
+  transform: skew(30deg, 0deg);
+
+}`;
+const Header =styled.h1`{
+  animation: ${rotate} infinite 30s linear;
+  text-align:center;
+  margin:2% auto;
+  color:white;
+  
+  text-shadow: 3px 3px 3px blue,
+                3px 3px 3px blue, 
+                3px 3px 3px  blue;
+  position:absolute;
+  top:12px;
+  left:200px
+}`;
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
@@ -8,10 +42,33 @@ const App = () => {
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
-
+  const [people, setPeople] = useState([]);
+   useEffect(() => {
+  axios
+  .get(`https://swapi.co/api/people/`)
+  .then(response => {
+    const characters = response.data.results;
+    console.log(response.data.results);
+    setPeople(characters);
+    
+  })
+  .catch(error => {
+    console.log("Sorry no people", error);
+  });
+ },[]);
   return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
+    <div>
+    <Header>
+      <p>React Wars</p>
+      <p>StarWars Characters</p>
+    </Header>
+    <Container>
+      
+      {people.map((person,index) => {
+          return <DisplayInfo key={index} character={person} />;
+        })}
+    </Container>
+      
     </div>
   );
 }
