559d8872cbe52cfcd990cd918ab0be72c4399609
shiva banihashem <sbanihas@gmail.com>
2019-09-28 15:46:40 -0500

    Stretch Pagination included

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import axios from 'axios';
 import DisplayInfo from './components/DisplayInfo.js';
 import styled,{ keyframes } from 'styled-components';
+import Pagination from './components/Pagination.js';
 
 const Container = styled.div`
 width: 98%;
@@ -43,6 +44,9 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   const [people, setPeople] = useState([]);
+  const [currentPage,setCurrentPage]= useState(1);
+  const [postPerPage] = useState(3);
+
    useEffect(() => {
   axios
   .get(`https://swapi.co/api/people/`)
@@ -56,6 +60,13 @@ const App = () => {
     console.log("Sorry no people", error);
   });
  },[]);
+
+ const indexOfLastPost = currentPage*postPerPage;
+ const indexOfFirstPost = indexOfLastPost - postPerPage;
+ const currentPeople = people.slice(indexOfFirstPost ,indexOfLastPost);
+
+ const paginate = (pageNumber)=> setCurrentPage(pageNumber);
+
   return (
     <div>
     <Header>
@@ -64,10 +75,13 @@ const App = () => {
     </Header>
     <Container>
       
-      {people.map((person,index) => {
+      {currentPeople.map((person,index) => {
           return <DisplayInfo key={index} character={person} />;
         })}
     </Container>
+    <Pagination postsPerPage={postPerPage} totalPosts={people.length}
+                paginate = {paginate}>
+    </Pagination>
       
     </div>
   );
